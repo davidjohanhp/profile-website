7d7072dab65194daf394cda4ce7a5b73c971d577
David Johan Hasiholan Parhusip <david.johanhp@gmail.com>
2023-06-16 17:13:53 +0700

    about: add skills slider

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -71,7 +71,7 @@ class About extends React.Component {
                     <div>
                         <h2 className="text-start font-semibold text-gray-900 sm:text-3xl">Experienced in</h2>
                         <div className="pt-4">
-                            {/* <Slider {...this.settings}>
+                            <Slider {...this.settings}>
                                 {this.state.images.map(image => {
                                     return (
                                         <div className="flex">
@@ -81,7 +81,7 @@ class About extends React.Component {
                                         </div>
                                         );
                                     })}
-                            </Slider> */}
+                            </Slider>
                         </div>
                     </div>
                 </div>
